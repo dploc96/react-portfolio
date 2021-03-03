10bb6851f1b3e3c80f68ac9a7f25fee66d57dc60
dploc96 <mikhailcolt@gmail.com>
2021-03-03 12:28:04 +0700

    updating section pet project

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import './Project.scss';
-import { Link } from 'react-router-dom';
 
 export default function Project() {
   return (
     <section id="project">
       <h2 className="title title--project">Pet projects</h2>
       <div className="project__list">
-        <a href="/birthday-reminder" className="project__item">
-          <img src="images/portfolio.jpg" alt="" />
-        </a>
-
-        <a
-          href="https://frozen-crag-64962.herokuapp.com/"
-          className="project__item"
-        >
-          <img src="images/portfolio.jpg" alt="" />
-        </a>
-
-        <a href="/flappy-bird" className="project__item">
-          <img src="images/portfolio.jpg" alt="" />
-        </a>
-
-        <a href="portfolio-item.html" className="project__item">
-          <img src="images/portfolio.jpg" alt="" />
-        </a>
+        <div className="project__item">
+          <a href="https://dploc96.github.io/vanilla-js-band.github.io/">
+            <img src="images/theBand.png" alt="" />
+          </a>
+          <h4>The Band</h4>
+        </div>
+        <div className="project__item">
+          <a href="https://frozen-crag-64962.herokuapp.com/">
+            <img src="images/amazonClone.png" alt="" />
+          </a>
+          <h4>Amazon Simple Clone</h4>
+        </div>
+        {/* <div className="project__item">
+          <a href="/flappy-bird">
+            <img src="images/portfolio.jpg" alt="" />
+          </a>
+        </div>
+        <div className="project__item">
+          <a href="portfolio-item.html">
+            <img src="images/portfolio.jpg" alt="" />
+          </a>
+        </div> */}
       </div>
     </section>
   );
